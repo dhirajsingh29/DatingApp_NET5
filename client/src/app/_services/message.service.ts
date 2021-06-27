7ed9c673e962a0198f04e40b5fb62ebbb595df5d
dhirajsingh29 <dhirajsingh_29@outlook.com>
2021-06-27 20:15:55 +0530

    Applied Messages Fix

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -61,6 +61,7 @@ export class MessageService {
 
   stopHubConnection() {
     if (this.hubConnection) {
+      this.messageThreadSource.next([]);
       this.hubConnection.stop();
     }
   }
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -14,6 +14,7 @@ export class MemberMessagesComponent implements OnInit {
   @Input() messages: Message[];
   @Input() username: string;
   messageContent: string;
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -25,10 +26,11 @@ export class MemberMessagesComponent implements OnInit {
     //   this.messages.push(message);
     //   this.messageForm.reset();
     // })
+    this.loading = true;
 
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm.reset();
-    })
+    }).finally(() => this.loading = false);
   }
 
 }
